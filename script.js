46d995b1568c4238193a4ad3f421b0041c948a38
Parv <parvruhil68@gmail.com>
2024-08-26 16:41:20 +0530

    js file 2

--- script.js
+++ script.js
@@ -56,8 +56,25 @@ class Player {
   }
 }
 
-const player = new Player();
 
+class Platform {
+  constructor(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 200;
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = '#acd157'
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height
+    )
+  }
+}
+
+
+const player = new Player();
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -112,11 +129,18 @@ const movePlayer = (key, xVelocity, isPressed) => {
   }
 }
 
-
 const startGame = () => {
   canvas.style.display = "block";
   startScreen.style.display = "none";
-  player.draw();
+  animate();
 }
 
 startBtn.addEventListener("click", startGame);
+
+window.addEventListener("keydown", ({ key }) => {
+  movePlayer(key, 8, true);
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  movePlayer(key, 0, false);
+});
