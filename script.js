9f4dc3bdd0d4433bc10ad0c3f202f167f9e8cb48
Parv <parvruhil68@gmail.com>
2024-08-27 16:19:10 +0530

    js file update

--- script.js
+++ script.js
@@ -71,6 +71,30 @@ class Platform {
   }
 }
 
+class CheckPoint {
+  constructor(x, y, z) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(70);
+    this.claimed = false;
+  };
+
+  draw() {
+    ctx.fillStyle = "#f1be32";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  claim() {
+    this.width = 0;
+    this.height = 0;
+    this.position.y = Infinity;
+    this.claimed = true;
+  }
+};
+
+
 const player = new Player();
 
 const platformPositions = [
@@ -91,6 +115,8 @@ const platformPositions = [
 const platforms = platformPositions.map(
   (platform) => new Platform(platform.x, platform.y)
 );
+
+
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -129,9 +155,24 @@ const animate = () => {
         platform.position.x + platform.width - player.width / 3,
     ];
 
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
 
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+
+    if (platformDetectionRules.every(rule => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
+    };
   });
-
 }
 
 
